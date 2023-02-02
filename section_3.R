dedd7402c32bad47e2cd7f9661ab66c647921ba9
William Lai <williamlai5@gmail.com>
2023-02-02 22:55:21 +1100

    section3

---
+++ section_3.R
@@ -0,0 +1,13 @@
+# libraries-----------------------------------------------------------
+source("NADA2_online_3_9/Class Data/Loadlibs.R")
+
+# some datasets in the NADA package -----------------------------------
+data("ShePyrene")
+ShePyrene
+
+
+data("Oahu")
+Oahu
+
+
+# skip the rest !!!
